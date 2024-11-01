8e6ba6874e280ba62069ee36f22d8e1acf6f2b22
Gilberto De Melo, Jr <gilberto.demelo@yahoo.com>
2024-11-01 10:55:10 -0300

    funcao de teste de insercao completada

--- TestePerformance.java
+++ TestePerformance.java
@@ -10,9 +10,37 @@ public class TestePerformance {
         avl = new AVL();
     }
 
-
     // métodos
+    public void testeInsercao(int[] data) {
 
+        long startTime, endTime;
+
+        // inserção na BST
+        startTime = System.nanoTime();
+        for (int value : data) {
+            bst.insert(value);
+        }
+        endTime = System.nanoTime();
+        System.out.println("Tempo de inserção BST: " + (endTime - startTime) + " ns");
+
+
+        // inserção AVL
+        startTime = System.nanoTime();
+        for(int value : data) {
+            avl.insertAVL(value);
+        }
+        endTime = System.nanoTime();
+        System.out.println("Tempo de inserção AVL: " + (endTime - startTime) + " ns");
+
+    }
+
+    public void testeBusca(int[] data) {
+
+        long startTime, endTime;
+
+        // busca
+
+    }
 
 
 } // TestePerformance
