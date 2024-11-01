715454050fabab4689897b6f122abadb87b81ade
Gilberto De Melo, Jr <gilberto.demelo@yahoo.com>
2024-11-01 09:28:16 -0300

    classe para realizar testes de comparações entre AVL e BST

---
+++ TestePerformance.java
@@ -0,0 +1,18 @@
+public class TestePerformance {
+
+    // atributos
+    private BST bst;
+    private AVL avl;
+
+    // construtor
+    public TestePerformance() {
+        bst = new BST();
+        avl = new AVL();
+    }
+
+
+    // métodos
+
+
+
+} // TestePerformance
